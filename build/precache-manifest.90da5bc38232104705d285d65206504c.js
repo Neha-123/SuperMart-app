61f758d4755b7a6aa99b09018728120183e295d6
unknown <nehasingh1252@gmail.com>
2020-10-19 00:50:28 +0530

    add build

---
+++ build/precache-manifest.90da5bc38232104705d285d65206504c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3bde2bbe162203f4523f09f14ef6bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759cdbcc14b5f5448aa9",
+    "url": "/static/css/main.e79da9f3.chunk.css"
+  },
+  {
+    "revision": "d201b42744e5289b1cf4",
+    "url": "/static/js/2.239b417d.chunk.js"
+  },
+  {
+    "revision": "7780c824e7f76157088dfeaa67c9ab9e",
+    "url": "/static/js/2.239b417d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759cdbcc14b5f5448aa9",
+    "url": "/static/js/main.05951ce9.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
